01666dd520e258962ca886fdba1b61d83c1573a1
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-03-30 14:52:22 +0200

    Envoi du premier commit avec les répertoires PHP JS HTML CSS et Contenus

---
+++ Jeu/PHP/PageSimple.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<HTML>
+
+	<HEAD> <!--Information de configuration : encodage, langue,...-->
+		<!--Méthode d'insertion du style dans le HEAD-->
+		<style type="text/css"></style>
+		<!--Appelle du style par un fichier externe.-->
+		<link rel="stylesheet" type="text/css" href="../CSS/Index.CSS" charset="utf-8"/>
+		<link rel="shortcut icon" type="image/x-icon" href="../Contenus/images/cirno9.jpg"/>
+		<meta charset="utf-8"/>
+		<lang="fr"/>
+		<TITLE>Page Simple</TITLE>
+	</HEAD>
+
+	<BODY>
+		<h1>Ceci est une page PHP.</h1>
+
+		<footer>
+			Contact : r.schlotter@ludus-académie.com<br/>
+			Téléphone : +33624202366
+		</footer>	
+	</BODY>
+</HTML>
---
+++ Jeu/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<HTML>
+
+	<HEAD> <!--Information de configuration : encodage, langue,...-->
+		<!--Méthode d'insertion du style dans le HEAD-->
+		<style type="text/css"></style>
+		<!--Appelle du style par un fichier externe.-->
+		<link rel="stylesheet" type="text/css" href="./CSS/Index.CSS" charset="utf-8"/>
+		<link rel="shortcut icon" type="image/x-icon" href="../Contenus/images/cirno9.jpg"/>
+		<meta charset="utf-8"/>
+		<lang="fr"/>
+		<TITLE>Index du Jeu</TITLE>
+	</HEAD>
+
+	<BODY>
+		<h1>Index du Jeu</h1>
+
+		<h2>Cette page sert d'index pour accéder aux divers page de notre hôte virtuel !</h2>
+
+		<a href="PHP/PageSimple.php">Page Simple</a><br/>
+
+		<footer>
+			Contact : r.schlotter@ludus-académie.com<br/>
+			Téléphone : +33624202366
+		</footer>	
+	</BODY>
+</HTML>
